88798b327bef1b7e3d7ed5a0244724f9891df675
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-04 06:10:41 +0100

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -20,5 +20,42 @@ created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
 updated_at: TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP
   );
 
+CREATE TABLE Booking (
+  Booking
+booking_id: Primary Key, UUID, Indexed
+property_id: Foreign Key, references Property(property_id)
+user_id: Foreign Key, references User(user_id)
+start_date: DATE, NOT NULL
+end_date: DATE, NOT NULL
+total_price: DECIMAL, NOT NULL
+status: ENUM (pending, confirmed, canceled), NOT NULL
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE Payment (
+  payment_id: Primary Key, UUID, Indexed
+booking_id: Foreign Key, references Booking(booking_id)
+amount: DECIMAL, NOT NULL
+payment_date: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+payment_method: ENUM (credit_card, paypal, stripe), NOT NULL
+  );
+
+CREATE TABLE Review (
+  Review
+review_id: Primary Key, UUID, Indexed
+property_id: Foreign Key, references Property(property_id)
+user_id: Foreign Key, references User(user_id)
+rating: INTEGER, CHECK: rating >= 1 AND rating <= 5, NOT NULL
+comment: TEXT, NOT NULL
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+  );
+
+CREATE TABLE Message (
+  message_id: Primary Key, UUID, Indexed
+sender_id: Foreign Key, references User(user_id)
+recipient_id: Foreign Key, references User(user_id)
+message_body: TEXT, NOT NULL
+sent_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+  );
 
   

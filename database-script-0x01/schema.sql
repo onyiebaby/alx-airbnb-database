4107f481b5137e916d4ebfd99c5df1d6b66488c0
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-04 06:04:11 +0100

    Update schema.sql

--- database-script-0x01/schema.sql
+++ database-script-0x01/schema.sql
@@ -1,1 +1,24 @@
+CREATE TABLE Users (
+  user_id: Primary Key, UUID, Indexed
+first_name: VARCHAR, NOT NULL
+last_name: VARCHAR, NOT NULL
+email: VARCHAR, UNIQUE, NOT NULL
+password_hash: VARCHAR, NOT NULL
+phone_number: VARCHAR, NULL
+role: ENUM (guest, host, admin), NOT NULL
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+);
 
+CREATE TABLE Property (
+  property_id: Primary Key, UUID, Indexed
+host_id: Foreign Key, references User(user_id)
+name: VARCHAR, NOT NULL
+description: TEXT, NOT NULL
+location: VARCHAR, NOT NULL
+pricepernight: DECIMAL, NOT NULL
+created_at: TIMESTAMP, DEFAULT CURRENT_TIMESTAMP
+updated_at: TIMESTAMP, ON UPDATE CURRENT_TIMESTAMP
+  );
+
+
+  

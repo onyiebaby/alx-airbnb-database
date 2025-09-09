79de18b9058e82c0ba31dedbe10441cce1b2ded8
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-09 20:11:33 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,10 @@
+-- Total number of bookings per user
+SELECT 
+    u.user_id,
+    u.name AS user_name,
+    COUNT(b.booking_id) AS total_bookings
+FROM users u
+LEFT JOIN bookings b ON u.user_id = b.user_id
+GROUP BY u.user_id, u.name
+ORDER BY total_bookings DESC;
 

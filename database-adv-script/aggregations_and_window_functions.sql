c054fb27a4b0acdbe40397555caeedf0dba5d4fe
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-09 20:23:47 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -8,3 +8,16 @@ LEFT JOIN bookings b ON u.user_id = b.user_id
 GROUP BY u.user_id, u.name
 ORDER BY total_bookings DESC;
 
+
+-- Ranking properties by total number of bookings
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS row_number_rank,
+    RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS rank_position
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY total_bookings DESC;
+

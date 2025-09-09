6b625e549bccc051b1f9a035578f21226c1c3154
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-09 19:19:12 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,9 +4,16 @@ FROM Bookings b
 INNER JOIN Users u ON b.user_id = u.user_id;
 
 -- LEFT JOIN: Retrieve all properties and their reviews including properties that have no reviews.
-SELECT p.property_id, p.name AS property_name, r.reviews_id, r.rating, r.comment
-FROM Property p
-LEFT JOIN Reviews r ON p.property_id = r.property_id;
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM properties p
+LEFT JOIN reviews r 
+    ON p.property_id = r.property_id;
+
 
 -- FULL OUTER JOIN: Retrieve all users and all bookings even if the user has no booking or a booking is not linked to a user
 SELECT u.users_id, u.name AS user_name, b.booking_id, b.property_id, b.check_in, b.check_out

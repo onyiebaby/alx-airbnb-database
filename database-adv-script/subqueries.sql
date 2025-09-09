0ff6f4e0ce1976a668f097a0eb8678d7aac513f1
Onyinyechi Promise Matthew <onyinyechimatthew06@gmail.com>
2025-09-09 20:06:21 +0100

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,20 +1,20 @@
 -- Non-Correlated Subquery:
--- Find all properties where the average rating is greater than 4.0
-SELECT p.property_id, p.name AS property_name
-FROM Properties p
-WHERE p.property_id IN (
-  SELECT r.property_id
-  FROM Reviews r
-  GROUP BY r.property_id
-  HAVING AVG(r.rating) > 4.0
+-- Non-correlated subquery: Properties with average rating > 4.0
+SELECT property_id, name AS property_name
+FROM properties
+WHERE property_id IN (
+    SELECT property_id
+    FROM reviews
+    GROUP BY property_id
+    HAVING AVG(rating) > 4.0
 );
 
 --Correlated Subquery:
---Find users who have made more than 3 bookings
+-- Correlated subquery: Users with more than 3 bookings
 SELECT u.user_id, u.name AS user_name
-FROM Users u
+FROM users u
 WHERE (
-  SELECT COUNT(*)
-  FROM Bookings b
-  WHERE b.user_id = u.user_id
+    SELECT COUNT(*)
+    FROM bookings b
+    WHERE b.user_id = u.user_id
 ) > 3;
